e1f3eb44578d62f0a1962960bd87c2b4d022fcf0
Josh Lemer <Josh.Lemur@marriott-sp.com>
2019-11-25 23:40:10 -0600

    added scaladocs

--- src/main/scala/com/github/joshlemer/clxn/unsafewrap/unsafewrap.scala
+++ src/main/scala/com/github/joshlemer/clxn/unsafewrap/unsafewrap.scala
@@ -6,6 +6,19 @@ import scala.collection.immutable.{SeqMap, SortedMap, SortedSet}
 package object unsafewrap {
 
   implicit final class IterableObjectExtension(private val c: Iterable.type) extends AnyVal {
+    /** Produces an immutable Iterable from any collection.Iterable, without performing any copying.
+      *
+      * If the passed Iterable is already immutable, no wrapping is applied.
+      *
+      * Otherwise, an immutable wrapping Iterable will be returned which delegates to the passed
+      * Iterable for all operations. Any further mutations performed on the passed Iterable will
+      * result in undefined behaviour and is considered user error -- changes may or may not be
+      * reflected in the resulting immutable.Iterable
+      *
+      * @param it the Iterable to wrap as immutable
+      * @tparam A the type of element the Iterable contains
+      * @return an immutable Iterable containing the same data as the passed Iterable
+      */
     def unsafeWrap[A](it: collection.Iterable[A]): Iterable[A] = it match {
       case xs: Iterable[A] => xs
       case xs if xs.knownSize == 0 => Nil
@@ -19,6 +32,19 @@ package object unsafewrap {
 
 
   implicit final class SeqObjectExtension(private val c: Seq.type) extends AnyVal {
+    /** Produces an immutable Seq from any collection.Seq, without performing any copying.
+      *
+      * If the passed Seq is already immutable, no wrapping is applied.
+      *
+      * Otherwise, an immutable wrapping Seq will be returned which delegates to the passed
+      * Seq for all operations. Any further mutations performed on the passed Seq will
+      * result in undefined behaviour and is considered user error -- changes may or may not be
+      * reflected in the resulting immutable.Seq
+      *
+      * @param it the Seq to wrap as immutable
+      * @tparam A the type of element the Seq contains
+      * @return an immutable Seq containing the same data as the passed Seq
+      */
     def unsafeWrap[A](it: collection.Seq[A]): Seq[A] = it match {
       case xs: IndexedSeq[A] => new IndexedSeqWrapper(xs)
       case xs: Seq[A] => xs
@@ -34,6 +60,19 @@ package object unsafewrap {
   }
 
   implicit final class IndexedSeqObjectExtension(private val is: IndexedSeq.type) extends AnyVal {
+    /** Produces an immutable IndexedSeq from any collection.IndexedSeq, without performing any copying.
+      *
+      * If the passed IndexedSeq is already immutable, no wrapping is applied.
+      *
+      * Otherwise, an immutable wrapping IndexedSeq will be returned which delegates to the passed
+      * IndexedSeq for all operations. Any further mutations performed on the passed IndexedSeq will
+      * result in undefined behaviour and is considered user error -- changes may or may not be
+      * reflected in the resulting immutable.IndexedSeq
+      *
+      * @param it the IndexedSeq to wrap as immutable
+      * @tparam A the type of element the IndexedSeq contains
+      * @return an immutable IndexedSeq containing the same data as the passed IndexedSeq
+      */
     def unsafeWrap[A](it: collection.IndexedSeq[A]): IndexedSeq[A] = it match {
       case xs: IndexedSeq[A] => xs
       case xs if xs.isEmpty => Vector.empty
@@ -47,6 +86,19 @@ package object unsafewrap {
   }
 
   implicit final class SetObjectExtension(private val c: Set.type) extends AnyVal {
+    /** Produces an immutable Set from any collection.Set, without performing any copying.
+      *
+      * If the passed Set is already immutable, no wrapping is applied.
+      *
+      * Otherwise, an immutable wrapping Set will be returned which delegates to the passed
+      * Set for all operations. Any further mutations performed on the passed Set will
+      * result in undefined behaviour and is considered user error -- changes may or may not be
+      * reflected in the resulting immutable.Set
+      *
+      * @param it the Set to wrap as immutable
+      * @tparam A the type of element the Set contains
+      * @return an immutable Set containing the same data as the passed Set
+      */
     def unsafeWrap[A](it: collection.Set[A]): Set[A] = it match {
       case xs: Set[A] => xs
       case xs if xs.isEmpty => Set.empty
@@ -62,6 +114,19 @@ package object unsafewrap {
   }
 
   implicit final class SortedSetObjectExtension(private val c: SortedSet.type) extends AnyVal {
+    /** Produces an immutable SortedSet from any collection.SortedSet, without performing any copying.
+      *
+      * If the passed SortedSet is already immutable, no wrapping is applied.
+      *
+      * Otherwise, an immutable wrapping SortedSet will be returned which delegates to the passed
+      * SortedSet for all operations. Any further mutations performed on the passed SortedSet will
+      * result in undefined behaviour and is considered user error -- changes may or may not be
+      * reflected in the resulting immutable.SortedSet
+      *
+      * @param it the SortedSet to wrap as immutable
+      * @tparam A the type of element the SortedSet contains
+      * @return an immutable SortedSet containing the same data as the passed SortedSet
+      */
     def unsafeWrap[A](it: collection.SortedSet[A]): Set[A] = it match {
       case xs: SortedSet[A] => xs
       case xs if xs.isEmpty => SortedSet.empty(xs.ordering)
@@ -85,6 +150,20 @@ package object unsafewrap {
   }
 
   implicit final class MapObjectExtension(private val c: Map.type) extends AnyVal {
+    /** Produces an immutable Map from any collection.Map, without performing any copying.
+      *
+      * If the passed Map is already immutable, no wrapping is applied.
+      *
+      * Otherwise, an immutable wrapping Map will be returned which delegates to the passed
+      * Map for all operations. Any further mutations performed on the passed Map will
+      * result in undefined behaviour and is considered user error -- changes may or may not be
+      * reflected in the resulting immutable.Map
+      *
+      * @param it the Map to wrap as immutable
+      * @tparam K the type of key Map contains
+      * @tparam V the type of key Map contains
+      * @return an immutable Map containing the same data as the passed Map
+      */
     def unsafeWrap[K, V](it: collection.Map[K, V]): Map[K, V] = it match {
       case xs: Map[K, V] => xs
       case xs if xs.isEmpty => Map.empty
@@ -100,6 +179,20 @@ package object unsafewrap {
   }
 
   implicit final class SortedMapObjectExtension(private val c: SortedMap.type) extends AnyVal {
+    /** Produces an immutable SortedMap from any collection.SortedMap, without performing any copying.
+      *
+      * If the passed SortedMap is already immutable, no wrapping is applied.
+      *
+      * Otherwise, an immutable wrapping SortedMap will be returned which delegates to the passed
+      * SortedMap for all operations. Any further mutations performed on the passed SortedMap will
+      * result in undefined behaviour and is considered user error -- changes may or may not be
+      * reflected in the resulting immutable.SortedMap
+      *
+      * @param it the SortedMap to wrap as immutable
+      * @tparam K the type of key Map contains
+      * @tparam V the type of key Map contains
+      * @return an immutable SortedMap containing the same data as the passed Map
+      */
     def unsafeWrap[K, V](it: collection.SortedMap[K, V]): SortedMap[K, V] = it match {
       case xs: SortedMap[K, V] => xs
       case xs if xs.isEmpty => SortedMap.empty(it.ordering)
@@ -128,6 +221,20 @@ package object unsafewrap {
   }
 
   implicit final class SeqMapObjectExtension(private val c: SeqMap.type) extends AnyVal {
+    /** Produces an immutable SeqMap from any collection.SeqMap, without performing any copying.
+      *
+      * If the passed SeqMap is already immutable, no wrapping is applied.
+      *
+      * Otherwise, an immutable wrapping SeqMap will be returned which delegates to the passed
+      * SeqMap for all operations. Any further mutations performed on the passed SeqMap will
+      * result in undefined behaviour and is considered user error -- changes may or may not be
+      * reflected in the resulting immutable.SeqMap
+      *
+      * @param it the SeqMap to wrap as immutable
+      * @tparam K the type of key Map contains
+      * @tparam V the type of key Map contains
+      * @return an immutable SeqMap containing the same data as the passed Map
+      */
     def unsafeWrap[K, V](it: collection.SeqMap[K, V]): SeqMap[K, V] = it match {
       case xs: SeqMap[K, V] => xs
       case xs if xs.isEmpty => SeqMap.empty
